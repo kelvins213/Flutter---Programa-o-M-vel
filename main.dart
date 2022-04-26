e54d02578f1624da79dc656a51348d942ecb6f96
kelvins213 <103438311+kelvins213@users.noreply.github.com>
2022-04-26 08:42:39 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp( //classe MaterialApp  (widget)
+      home: Scaffold( //classe Scaffold  (widget)
+        body: Center( //classe Center  (widget)
+          child: Text ( //classe Text (widget)
+            "Hello, World",
+                style: TextStyle (fontSize: 40),//classe TextStyle (widget)
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+//O flutter trará duas postas: um android, e outra IOS.
+//A pasta lib será o diretório de código principal.
+//A pasta build, por sua vez, terá o código compilado.
+//Pubspec.lock: terá as bibliotecas que iremos usar.
+
+//runApp ();// => Classe Padrão do Flutter.udo no flutter é um Widget (qualquer peça de interface será um Widget).
+//Junta-se os widgets para criar os aplicativo.
+//Widget, no flutter, sempre será uma classe. Dessa classe, as outras herdarão as
+//características de suas interfaces.
+//Os aplicativos serão construídos com base em árvores de Widgets.
+//Essas árvores podem desempenhar várias funções: estrutura, layout, interface, etc.
+//No canal do Flutter, eles ensinam como os widgets funcionam. Link: https://www.youtube.com/c/flutterdev/playlists
+//MaterialApp => Android;  CupertinoApp => IOS.
+//Sempre que você abrir o androidstudio, o flutter gerará um aplicativo automático.
+// => Trazer o cabo do celular na aula da semana que vem.
+
+//O widget Scaffold trará a estrutura da tela já feita.
+//Para criar uma nova tela, sempre será necessário adicionar um Scaffold.
+//Um Scaffold por tela.
